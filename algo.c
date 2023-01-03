d1cce4b6a03e72ca1a43c4af4aca4c65581ccd94
Kigotrunk <117681271+Kigotrunk@users.noreply.github.com>
2023-01-03 13:14:24 +0100

    Add files via upload

---
+++ algo.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kortolan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/03 11:43:28 by kortolan          #+#    #+#             */
+/*   Updated: 2023/01/03 12:20:23 by kortolan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void up_in_a(Element **a, Element **b)
+{
+	while (a->suivant->nombre < b->nombre)
+		ra
+	pa(a, b);
+}
+
+void down_in_a(Element **a, Element **b)
+{
+
+}
+
+
+int	step_for_one(Element *a, Element *b)
+{
+
+}
